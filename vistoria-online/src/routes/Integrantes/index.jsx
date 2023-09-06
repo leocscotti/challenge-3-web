6510af9b1ea919066b95d3cd6b0e2990ce5cf31c
mariialbano <marianatalbano@gmail.com>
2023-09-06 11:03:44 -0300

    estrutura inicial

---
+++ vistoria-online/src/routes/Integrantes/index.jsx
@@ -0,0 +1,8 @@
+export default function Integrantes(){
+
+    return(
+        <main>
+            <h1>Integrantes</h1>
+        </main>
+    )
+}
---
+++ vistoria-online/src/routes/Vistoria/index.jsx
@@ -0,0 +1,8 @@
+export default function Vistoria(){
+
+    return(
+        <main>
+            <h1>Vistoria</h1>
+        </main>
+    )
+}
