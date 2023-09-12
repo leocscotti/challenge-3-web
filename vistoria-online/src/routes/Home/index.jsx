09905ac3daf62bdef383a443ad7e3a0044d42785
leocscotti <leoscotti03@gmail.com>
2023-09-12 17:25:34 -0300

    pagina home e menu atualizados

--- vistoria-online/src/routes/Home/index.jsx
+++ vistoria-online/src/routes/Home/index.jsx
@@ -1,3 +1,4 @@
+import LogoWhatsapp from '../../assets/images/logo-wpp.jpg'
 export default function Home(){
 
     return(
@@ -7,7 +8,12 @@ export default function Home(){
                 <p>A porto é mais que uma seguradora: é um ecossistema com todas as soluções para transformar sonhos em realidades diárias.</p>
                 <p>
                 Com mais de 75 anos de mercado, possui hoje três verticais de negócios: Porto Seguros, Porto Saúde e Porto Seguro Bank, formando, assim, um conjunto de soluções de proteção para os seus momentos que vão desde o dia a dia até o grande dia.</p>
+                <h2>Instruções para a vistoria</h2>
+                <p>Entre no link que será encaminhado via Whatsapp para realizar a vistoria</p>
+                <p>A vistoria deve ser feita diretamente por um celular com fotos tiradas na hora</p>
+                <p>Tenha o documento e/ou nota fiscal de sua bike por perto</p>
             </div>
+          
             
         </main>
     )
