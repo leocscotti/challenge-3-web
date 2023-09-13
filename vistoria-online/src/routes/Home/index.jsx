5dcb53db3ebbd083b00596ccf5e75e371ecbb57f
VitorKubica <vitor05kubica12@gmail.com>
2023-09-12 22:45:14 -0300

    Mudanças nas imagens e finalização do css

--- vistoria-online/src/routes/Home/index.jsx
+++ vistoria-online/src/routes/Home/index.jsx
@@ -6,7 +6,7 @@ export default function Home(){
         <main>
             <div>
                 <img src={LogoPorto} alt="LogoPorto" className='LogoPorto'/>
-                <h2>Quem somos?</h2>
+                <h1>Quem somos?</h1>
                 <p>A porto é mais que uma seguradora: é um ecossistema com todas as soluções para transformar sonhos em realidades diárias.</p>
                 <p>
                 Com mais de 75 anos de mercado, possui hoje três verticais de negócios: Porto Seguros, Porto Saúde e Porto Seguro Bank, formando, assim, um conjunto de soluções de proteção para os seus momentos que vão desde o dia a dia até o grande dia.</p>
